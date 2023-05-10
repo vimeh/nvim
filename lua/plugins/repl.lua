3d4465f639d7354d50eeccf8642d28ff86c5f93a
Vinay Mehta <vinaymehta.nyc@gmail.com>
2023-05-09 22:00:54 -0700

    keymaps for repl, and setting for ipython

--- lua/plugins/repl.lua
+++ lua/plugins/repl.lua
@@ -14,13 +14,17 @@ return {
               -- returns a table (see below)
               command = { "zsh" },
             },
-            python = require("iron.fts.python").ipython,
+            python = {
+              command = { "ipython", "--no-autoindent" },
+            },
           },
           -- open repl as a vertical split
           repl_open_cmd = require("iron.view").split("30%"),
         },
         -- Iron doesn't set keymaps by default anymore.
         -- You can set them here or manually add keymaps to the functions in iron.core
+        -- TODO: move these over to lazyvim keymaps. need to figure how to do map visual
+        -- and not just normal mode
         keymaps = {
           send_motion = "<space>rc",
           visual_send = "<space>rc",
@@ -44,5 +48,11 @@ return {
       })
     end,
     event = "VeryLazy",
+    keys = {
+      { "<space>rs", "<cmd>IronRepl<cr>", desc = "Open repl" },
+      { "<space>rr", "<cmd>IronRestart<cr>", desc = "Restart repl" },
+      { "<space>rf", "<cmd>IronFocus<cr>", desc = "Focus repl" },
+      { "<space>rh", "<cmd>IronHide<cr>", desc = "Hide repl" },
+    },
   },
 }
