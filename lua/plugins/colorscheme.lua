474e57f1c5bc39baeee16dc1ef781a313245e5fb
Vinay Mehta <vinaymehta.nyc@gmail.com>
2023-05-09 21:40:20 -0700

    colors

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+        dim_inactive = { enabled = true, shade = "dark", percentage = 0.15 },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "HiPhish/nvim-ts-rainbow2",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        rainbow = {
+          enable = true,
+          -- list of languages you want to disable the plugin for
+          disable = { "jsx", "cpp" },
+          -- Which query to use for finding delimiters
+          query = "rainbow-parens",
+          -- Highlight the entire buffer all at once
+          strategy = require("ts-rainbow.strategy.global"),
+        },
+      })
+    end,
+    lazy = true,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+    lazy = true,
+  },
+}
