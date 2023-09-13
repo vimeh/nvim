add705c604b5620002c07a9306994c41d91ac40a
vimeh <vinay.mehta@getshippy.com>
2023-09-13 09:35:49 -0700

    add markdown preview plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    lazy = true,
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_preview_options = {
+        maid = {},
+        content_editable = true,
+      }
+      vim.g.mkdp_markdown_css = "/Users/vinay/notes/md.css"
+    end,
+  },
+}
