83158c6491ee47f8e6a2123b96d9a553a7107254
Vinay Mehta <hi@vinaym.com>
2024-03-03 15:20:13 -0800

    replace chatgpt with oatmeal

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,12 +1,11 @@
 return {
-  "jackMort/ChatGPT.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("chatgpt").setup()
-  end,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
+  "dustinblackman/oatmeal.nvim",
+  cmd = { "Oatmeal" },
+  keys = {
+    { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+  },
+  opts = {
+    backend = "openai",
+    model = "gpt-4-turbo-preview",
   },
 }
