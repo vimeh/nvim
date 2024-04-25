94dbb2c5eeb9054e9daab66f2749c77528177dff
Vinay Mehta <vinay@lotus.local>
2024-04-24 23:00:08 -0700

    manually add copilot so that can remove from lualine

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.leap" },
