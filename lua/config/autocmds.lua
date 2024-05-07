e5108d022020907565e6f71098ecba348677ef70
vimeh <vinay.mehta@getshippy.com>
2024-05-06 17:43:28 -0700

    add htmldjango to list of auto start copilot

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- enable Copilot automatically for lua and python
+-- enable Copilot automatically for some filetypes
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "lua", "python", "rust" },
+  pattern = { "lua", "python", "rust", "htmldjango" },
   callback = function()
     vim.cmd("Copilot enable")
   end,
