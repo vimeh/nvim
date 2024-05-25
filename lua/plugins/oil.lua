55013203c5e79d527d8c410a3f5f11a2279cf858
Vinay Mehta <email@vinaym.com>
2024-05-25 16:40:41 +0900

    add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      use_default_mappings = false,
+      columns = { "icon" },
+      view_options = { show_hidden = true },
+    },
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+  },
+}
