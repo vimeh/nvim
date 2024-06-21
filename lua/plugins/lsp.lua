5f545f4cbfdc22b8ea719b65ffed6941f3d68026
Vinay Mehta <email@vinaym.com>
2024-06-21 11:55:23 -0700

    add go, webdev lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,9 +7,7 @@ return {
         pyright = {},
         tsserver = {},
         eslint = {},
-        html = {
-          filetypes = { "html", "htmldjango", "templ" },
-        },
+        html = {},
         gopls = {
           gofumpt = true,
           staticcheck = true,
@@ -18,6 +16,10 @@ return {
             shadow = true,
           },
         },
+        tailwindcss = {},
+        htmx = {},
+        templ = {},
+        postgres_lsp = {},
       },
     },
     capabilities = {
@@ -28,6 +30,22 @@ return {
         },
       },
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.root_dir = opts.root_dir
+        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.fish_indent,
+        nls.builtins.diagnostics.fish,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.golines,
+        --nls.builtins.formatting.goimports_revisor,
+      })
+    end,
   },
   {
     "hedyhli/outline.nvim",
@@ -46,16 +64,18 @@ return {
     opts = {
       ensure_installed = {
         "bash",
+        "css",
+        "go",
         "html",
         "javascript",
         "json",
-        "go",
         "lua",
         "markdown",
         "markdown_inline",
         "python",
         "query",
         "regex",
+        "templ",
         "tsx",
         "typescript",
         "vim",
