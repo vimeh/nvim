d73b791647957587b2b9632b495bcee51021f1a5
vimeh <vinay.mehta@getshippy.com>
2024-06-21 11:15:47 -0700

    add eslint for frontend code; auto start with wrap enable

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@ vim.opt.breakindent = true
 vim.opt.linebreak = true
 
 vim.opt.diffopt:append("linematch:60")
+vim.opt.wrap = true
