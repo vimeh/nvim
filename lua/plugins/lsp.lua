56bc92b277e3427a51bd768f384bfa4930aa3833
Vinay Mehta <email@vinaym.com>
2024-11-23 12:51:16 -0800

    remove new defaults from config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,6 @@ return {
         ruff = {},
         pyright = {},
         tsserver = {},
-        eslint = {},
         html = {},
         gopls = {
           gofumpt = true,
@@ -41,22 +40,6 @@ return {
         },
       },
     },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.root_dir = opts.root_dir
-        or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.formatting.fish_indent,
-        nls.builtins.diagnostics.fish,
-        nls.builtins.formatting.stylua,
-        nls.builtins.formatting.shfmt,
-        nls.builtins.formatting.golines,
-        --nls.builtins.formatting.goimports_revisor,
-      })
-    end,
   },
   {
     "hedyhli/outline.nvim",
