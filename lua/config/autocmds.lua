0bbdea47fdcd40f9ac84d0beb50122d289d6591b
Adrian Wilkins <adrian.wilkins@gmail.com>
2024-12-11 16:56:39 +0000

    docs: Explain more about how to add and remove autocmds (#105)
    Co-authored-by: Adrian Wilkins <adrian.wilkins.contractor@dvla.gov.uk>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
 
 -- enable Copilot automatically for some filetypes
