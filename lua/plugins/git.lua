3e9b768950d78dcfa7681c06c61f9548ca5884e3
Vinay Mehta <hi@vinaym.com>
2025-01-01 20:19:45 -0800

    use git blame from gitsigns.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -32,17 +32,4 @@ return {
       },
     },
   },
-
-  -- git-blame
-  {
-    "f-person/git-blame.nvim",
-    keys = {
-      -- toggle needs to be called twice; https://github.com/f-person/git-blame.nvim/issues/16
-      { "<leader>gbe", ":GitBlameEnable<CR>", desc = "Blame line (enable)" },
-      { "<leader>gbd", ":GitBlameDisable<CR>", desc = "Blame line (disable)" },
-      { "<leader>gbs", ":GitBlameCopySHA<CR>", desc = "Copy SHA" },
-      { "<leader>gbc", ":GitBlameCopyCommitURL<CR>", desc = "Copy commit URL" },
-      { "<leader>gbf", ":GitBlameCopyFileURL<CR>", desc = "Copy file URL" },
-    },
-  },
 }
