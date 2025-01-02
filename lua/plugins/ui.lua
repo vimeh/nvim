21ed62ada23e1868959ce40413817541a5707004
Vinay Mehta <hi@vinaym.com>
2025-01-01 20:31:22 -0800

    remove inline fold

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,33 +27,4 @@ return {
       })
     end,
   },
-  {
-    "malbertzard/inline-fold.nvim",
-
-    opts = {
-      defaultPlaceholder = "…",
-      queries = {
-
-        -- Some examples you can use
-        html = {
-          { pattern = 'class="([^"]*)"', placeholder = "@" }, -- classes in html
-          { pattern = 'href="(.-)"' }, -- hrefs in html
-          { pattern = 'src="(.-)"' }, -- HTML img src attribute
-        },
-      },
-
-      jsx = {
-        { pattern = 'className="([^"]*)"' }, -- classes in jsx
-      },
-    },
-    keys = {
-      {
-        "<leader>uu",
-        function()
-          require("inline-fold").toggle()
-        end,
-        desc = "Toggle inline fold",
-      },
-    },
-  },
 }
