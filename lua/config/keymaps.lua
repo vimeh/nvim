755c26295d986a824fb1bfa5caca7681d37f7586
vimeh <vinay.mehta@getshippy.com>
2025-01-27 17:52:16 -0800

    use escape in terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- In the terminal, use <Esc> to exit insert mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
