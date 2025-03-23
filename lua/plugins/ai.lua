47a37d2d234a75c6eb3023d9c12e810d5adbc452
Vinay Mehta <hi@vinaym.com>
2025-03-22 22:10:36 -0700

    consolidate all the ai plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -31,6 +31,14 @@ return {
     },
   },
   {
+    "milanglacier/minuet-ai.nvim",
+    config = function()
+      require("minuet").setup({
+        -- Your configuration options here
+      })
+    end,
+  },
+  {
     "olimorris/codecompanion.nvim",
     config = true,
     dependencies = {
@@ -61,50 +69,165 @@ return {
       },
     },
   },
-  -- {
-  --   "ravitemer/mcphub.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
-  --   },
-  --   build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
-  --   config = function()
-  --     require("mcphub").setup({
-  --       -- Required options
-  --       port = 3000, -- Port for MCP Hub server
-  --       config = vim.fn.expand("~/mcpservers.json"), -- Absolute path to config file
-  --
-  --       -- Optional options
-  --       on_ready = function(hub)
-  --         -- Called when hub is ready
-  --       end,
-  --       on_error = function(err)
-  --         -- Called on errors
-  --       end,
-  --       shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
-  --       log = {
-  --         level = vim.log.levels.WARN,
-  --         to_file = false,
-  --         file_path = nil,
-  --         prefix = "MCPHub",
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+    config = function()
+      require("mcphub").setup({
+        -- Required options
+        port = 3000, -- Port for MCP Hub server
+        config = vim.fn.expand("~/mcpservers.json"), -- Absolute path to config file
+
+        -- Optional options
+        on_ready = function(hub)
+          -- Called when hub is ready
+        end,
+        on_error = function(err)
+          -- Called on errors
+        end,
+        shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
+        log = {
+          level = vim.log.levels.WARN,
+          to_file = false,
+          file_path = nil,
+          prefix = "MCPHub",
+        },
+      })
+    end,
+  },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      -- add any opts here
-      -- for example
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- timeout in milliseconds
-        temperature = 0, -- adjust if needed
+      ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+      -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+      -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+      -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+      auto_suggestions_provider = "claude",
+      cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20241022",
+        temperature = 0,
         max_tokens = 4096,
-        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      },
+      ---Specify the special dual_boost mode
+      ---1. enabled: Whether to enable dual_boost mode. Default to false.
+      ---2. first_provider: The first provider to generate response. Default to "openai".
+      ---3. second_provider: The second provider to generate response. Default to "claude".
+      ---4. prompt: The prompt to generate response based on the two reference outputs.
+      ---5. timeout: Timeout in milliseconds. Default to 60000.
+      ---How it works:
+      --- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively. Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output. Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+      ---Note: This is an experimental feature and may not work as expected.
+      dual_boost = {
+        enabled = false,
+        first_provider = "openai",
+        second_provider = "claude",
+        prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+        timeout = 60000, -- Timeout in milliseconds
+      },
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+        enable_token_counting = true, -- Whether to enable token counting. Default to true.
+        enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+        enable_claude_text_editor_tool_mode = false, -- Whether to enable Claude Text Editor Tool Mode.
+      },
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          all_theirs = "ca",
+          both = "cb",
+          cursor = "cc",
+          next = "]x",
+          prev = "[x",
+        },
+        suggestion = {
+          accept = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<C-s>",
+        },
+        sidebar = {
+          apply_all = "A",
+          apply_cursor = "a",
+          retry_user_request = "r",
+          edit_user_request = "e",
+          switch_windows = "<Tab>",
+          reverse_switch_windows = "<S-Tab>",
+          remove_file = "d",
+          add_file = "@",
+          close = { "<Esc>", "q" },
+          close_from_input = nil, -- e.g., { normal = "<Esc>", insert = "<C-d>" }
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        ---@type "right" | "left" | "top" | "bottom"
+        position = "right", -- the position of the sidebar
+        wrap = true, -- similar to vim.o.wrap
+        width = 30, -- default % based on available width
+        sidebar_header = {
+          enabled = true, -- true, false to enable/disable the header
+          align = "center", -- left, center, right for title
+          rounded = true,
+        },
+        input = {
+          prefix = "> ",
+          height = 8, -- Height of the input window in vertical layout
+        },
+        edit = {
+          border = "rounded",
+          start_insert = true, -- Start insert mode when opening the edit window
+        },
+        ask = {
+          floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+          start_insert = true, -- Start insert mode when opening the ask window
+          border = "rounded",
+          ---@type "ours" | "theirs"
+          focus_on_apply = "ours", -- which diff to focus after applying
+        },
+      },
+      highlights = {
+        ---@type AvanteConflictHighlights
+        diff = {
+          current = "DiffText",
+          incoming = "DiffAdd",
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        ---@type string | fun(): any
+        list_opener = "copen",
+        --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+        --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+        --- Disable by setting to -1.
+        override_timeoutlen = 500,
+      },
+      suggestion = {
+        debounce = 600,
+        throttle = 600,
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -147,6 +270,68 @@ return {
         },
         ft = { "markdown", "Avante" },
       },
+    },
+  },
+  {
+    "melbaldove/llm.nvim",
+    dependencies = { "nvim-neotest/nvim-nio" },
+    keys = {
+      {
+        "<leader>,",
+        function()
+          require("llm").prompt({ replace = false, service = "openai" })
+        end,
+        mode = "n",
+        desc = "Prompt LLM",
+      },
+      {
+        "<leader>,",
+        function()
+          require("llm").prompt({ replace = false, service = "openai" })
+        end,
+        mode = "v",
+        desc = "Prompt LLM",
+      },
+      {
+        "<leader>.",
+        function()
+          require("llm").prompt({ replace = true, service = "openai" })
+        end,
+        mode = "v",
+        desc = "Prompt LLM and replace",
+      },
+    },
+  },
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "llama3", -- The default model to use.
+      host = "localhost", -- The host running the Ollama service.
+      port = "11434", -- The port on which the Ollama service is listening.
+      quit_map = "q", -- set keymap for close the response window
+      retry_map = "<c-r>", -- set keymap to re-send the current prompt
+      init = function(options)
+        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+      end,
+      -- Function to initialize Ollama
+      command = function(options)
+        local body = { model = options.model, stream = true }
+        return "curl --silent --no-buffer -X POST http://"
+          .. options.host
+          .. ":"
+          .. options.port
+          .. "/api/chat -d $body"
+      end,
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a command string.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+      display_mode = "float", -- The display mode. Can be "float" or "split".
+      show_prompt = false, -- Shows the prompt submitted to Ollama.
+      show_model = false, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      debug = false, -- Prints errors and the command which is run.
     },
   },
 }
