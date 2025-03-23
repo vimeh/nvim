d0a00ca5e2d12ee8685ed774edcd2acc337a78e4
Vinay Mehta <hi@vinaym.com>
2025-03-22 23:41:10 -0700

    add biome for ts to conform

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,13 +4,15 @@ return {
     opts = {
       formatters_by_ft = {
         python = { "ruff_format", "ruff_lint", "ruff_organize_imports", "injected" },
-        javascript = { "biome", "injected" },
+        javascript = { "biome", "biome-check", "biome-organize-imports" },
+        typescript = { "biome", "biome-check", "biome-organize-imports" },
       },
       formatters = {
         injected = {
           options = {
             lang_to_formatters = {
               javascript = { "biome" },
+              typescript = { "biome" },
             },
           },
         },
