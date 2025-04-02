fdbf7dd74c4476a544803cb7fe6a2716e7a949a1
Vinay Mehta <hi@vinaym.com>
2025-04-01 23:49:00 -0700

    clean up llm

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -95,7 +95,6 @@ return {
         },
       },
       highlights = {
-        ---@type AvanteConflictHighlights
         diff = {
           current = "DiffText",
           incoming = "DiffAdd",
@@ -157,22 +156,4 @@ return {
       },
     },
   },
-  {
-    "Davidyz/VectorCode",
-    version = "*", -- optional, depending on whether you're on nightly or release
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "VectorCode", -- if you're lazy-loading VectorCode
-  },
-  --   {
-  --     "CopilotC-Nvim/CopilotChat.nvim",
-  --     dependencies = {
-  --       { "zbirenbaum/copilot.lua" },
-  --       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-  --     },
-  --     build = "make tiktoken", -- Only on MacOS or Linux
-  --     opts = {
-  --       -- See Configuration section for options
-  --     },
-  --     -- See Commands section for default commands if you want to lazy load on them
-  --   },
 }
