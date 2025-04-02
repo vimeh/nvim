74c88c519a87cfe152b86c1eed84f90781ba37eb
Vinay Mehta <hi@vinaym.com>
2025-04-01 23:06:06 -0700

    clean up colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,57 +1,14 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "macchiato",
-        transparent_background = false,
-        dim_inactive = { enabled = true, shade = "dark", percentage = 0.15 },
-        integrations = {
-          blink_cmp = true,
-          dadbod_ui = true,
-          dap = true,
-          dap_ui = true,
-          dashboard = true,
-          diffview = true,
-          fzf = true,
-          gitsigns = true,
-          grug_far = true,
-          leap = true,
-          lsp_trouble = true,
-          markdown = true,
-          mason = true,
-          neotree = true,
-          noice = true,
-          notify = true,
-          nvim_surround = true,
-          octo = true,
-          rainbow_delimiters = true,
-          render_markdown = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-          ufo = true,
-          which_key = true,
-          snacks = {
-            enabled = true,
-          },
-          mini = {
-            enabled = true,
-          },
-          navic = {
-            enabled = true,
-          },
-        },
-      })
-    end,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
     },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm", dim_inactive = true },
   },
   {
     "HiPhish/rainbow-delimiters.nvim",
